95fc75a92589b7a330f8f640c90fb660e0e12126
Aarón Palmer Peiró <aaronpalmerpeiro96@gmail.com>
2021-12-21 16:42:20 +0100

    Added comments in shared/Utils.ts

--- src/app/shared/Utils.ts
+++ src/app/shared/Utils.ts
@@ -2,6 +2,10 @@ import { FormGroup } from '@angular/forms';
 import * as CryptoJS from 'crypto-js';
 import { environment } from 'src/environments/environment';
 
+/**
+ * Reset errors and content from all controls of a FormGroup
+ * @param formGroup
+ */
 export const resetFrom = (formGroup: FormGroup) => {
   formGroup.reset();
 
@@ -10,16 +14,34 @@ export const resetFrom = (formGroup: FormGroup) => {
   });
 };
 
+/**
+ * Recover accessToken from sessionStorage
+ * @return accessToken from sessionStorage if exists
+ */
 export const recoverAccessToken = (): string | null => {
   return sessionStorage.getItem('accessToken');
 };
+
+/**
+ * Save accessToken in sessionStorage
+ * @param accessToken
+ */
 export const saveAccessToken = (accessToken: string): void => {
   sessionStorage.setItem('accessToken', accessToken);
 };
+
+/**
+ * Remove accessToken from sessionStorage
+ */
 export const removeAccessToken = (): void => {
   sessionStorage.removeItem('accessToken');
 };
 
+/**
+ * Encrypt an string
+ * @param password String to encrypt
+ * @return String encrypted
+ */
 export const encryptPassword = (password: string): string => {
   return CryptoJS.AES.encrypt(
     password.trim(),
@@ -27,6 +49,11 @@ export const encryptPassword = (password: string): string => {
   ).toString();
 };
 
+/**
+ * Decrypt an string
+ * @param password String to decrypted
+ * @return String decrypted
+ */
 export const decryptPassword = (password: string): string => {
   return CryptoJS.AES.decrypt(
     password.trim(),
